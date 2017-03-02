8a1736673d59af1f73272bcf46e4e9f5fd2c6531
Adi <crocplays26@gmail.com>
2017-03-02 09:43:06 +0200

    added multiple port scanning

--- TCPScan.py
+++ TCPScan.py
@@ -4,24 +4,32 @@ from scapy.all import *
 def isTCP(p):
     return TCP in p
 
-dstip='10.30.56.205'
-dstport=80
-pack = sr1(IP(dst= dstip)/TCP(dport = dstport),timeout = 10)
+def scan(ip,port):
+    """prints the status of a port. recieves ip,port """
+    dstip=ip
+    dstport=port
+    pack = sr1(IP(dst= dstip)/TCP(dport = dstport),timeout = 10)
+    
+    if ((pack[0].haslayer(TCP)==1)):
+        print " "
+        
+        if(pack[TCP].flags == 18) or (pack[TCP].flags == 16):
+            print "port " + str(dstport) +" is open"
+            openPorts.append(dstport)
+        elif((pack[TCP].flags == 4) or (pack[TCP].flags == 20) ):
+            print "port "+ str(dstport) + " is closed"
+        
 
+global openPorts
+openPorts = list()
 
-"""
-print pack
-print pack.show()
-print pack[TCP].flags
-"""
+targetIP = input("enter the target's ip address: ")
+#there are 65535 ports in a computer
+for i in range(200+1):
+    scan(targetIP,i)
+    
 
-
-if ((pack[0].haslayer(TCP)==1)):
-    print " "
-    if(pack[TCP].flags == 18) or (pack[TCP].flags == 16):
-        print "port " + str(dstport) +" is open"
-    elif((pack[TCP].flags == 4) or (pack[TCP].flags == 20) ):
-        print "port "+ str(dstport) + " is closed"
+print openPorts
 
 
 
